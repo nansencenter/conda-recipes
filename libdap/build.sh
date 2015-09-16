206aba7381c38013106367c1e4b87d6f02151b2c
Aleksander Vines <aleksandervines1@hotmail.com>
2015-09-16 11:53:55 +0200

    initial commit

---
+++ libdap/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#echo '----------------------------------------------------------'
+#pwd
+#echo "Builiding, Prefix:" + $PREFIX
+#echo "$INCLUDE_PATH:" $INCLUDE_PATH
+#ls
+
+#export SHELL=/bin/bash
+
+#export LIBCURL_CFLAGS=-I$PREFIX/include
+#export INCLUDE_PATH=$PREFIX/include
+#export CFLAGS="-I$PREFIX/include"
+#export CPPFLAGS="-I$PREFIX/include"
+#export LD_LIBRARY_PATH=$SYS_PREFIX/lib
+export LDFLAGS="-L$SYS_PREFIX/lib"
+export CPPFLAGS="-I$PREFIX/include"
+
+printenv
+#sed -i 's/\/bin\/sh/\/bin\/bash/g' ./aclocal.m4 ./conf/compile ./conf/config.guess ./conf/config.rpath ./conf/config.sub ./conf/depcomp ./conf/install-sh ./conf/libtool.m4 ./conf/ltmain.sh ./conf/missing ./configure ./dap-config-pkgconfig ./dap-config.in ./gl/config.charset ./gl/m4/lib-ld.m4 ./gl/m4/lib-link.m4 ./OSX_Resources/InstallationCheck ./tests/atconfig ./tests/DMRTest ./tests/DDSTest ./tests/DASTest ./tests/EXPRTest ./tests/getdapTest ./unit-tests/cache-testsuite/cleanup.sh.in 
+
+sed -i 's/\/bin\/sh/\/bin\/bash/g' configure
+
+bash configure \
+    --enable-threads=pth \
+    --prefix=$PREFIX
+
+#sed -i 's/\/bin\/sh/\/bin\/bash/g' Makefile
+
+make
+make install
---
+++ libgdal/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# http://www.michael-joost.de/gdal_install.html
+unset CC CPP CXX
+
+if [ `uname` == Darwin ]; then
+    export LDFLAGS="$LDFLAGS -headerpad_max_install_names"
+fi
+#force python bindings to not be built
+export PYTHON=
+
+sed -i 's/\/bin\/sh/\/bin\/bash/g' configure
+
+
+bash configure \
+    --without-python \
+    --with-hdf4=$PREFIX \
+    --with-hdf5=$PREFIX \
+    --with-netcdf=$PREFIX \
+    --with-dods-root=$PREFIX \
+    --with-geos=$PREFIX \
+    --prefix=$PREFIX
+
+
+make
+make install
+
+# Make sure GDAL_DATA and set and still present in the package
+# https://github.com/conda/conda-recipes/pull/267
+ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
+DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
+mkdir -p $ACTIVATE_DIR
+mkdir -p $DEACTIVATE_DIR
+
+cp $RECIPE_DIR/posix/activate.sh $ACTIVATE_DIR/gdal-activate.sh
+cp $RECIPE_DIR/posix/deactivate.sh $DEACTIVATE_DIR/gdal-deactivate.sh
+
